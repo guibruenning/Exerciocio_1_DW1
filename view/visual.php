d677b2c8e2789ba3d5f2867ee764c4472a9259b2
Bruenning <gui.h.bruenning@gmail.com>
2021-05-09 18:26:15 -0300

    iniciando

---
+++ view/visual.php
@@ -0,0 +1,11 @@
+<form action="" >
+    <button id="buttonClick btn btn-primary" nome="buttonClick" class="buttonClick">Rolar Dados</button>
+</form>
+
+<script>
+
+    $(".buttonClick").click(()=>{
+        
+    })
+
+</script>
