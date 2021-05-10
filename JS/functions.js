4b2e76c1914dae1802b6b9388c33821dc01a30dc
Bruenning <gui.h.bruenning@gmail.com>
2021-05-09 21:38:57 -0300

    calculando quem é o vencedor e tantando deixar com que um dado aparece em sequencia, não todos ao mesmo tempo

--- JS/functions.js
+++ JS/functions.js
@@ -1,3 +1,8 @@
+var ran_1 = 0
+var ran_2 = 0
+var ran_3 = 0
+var ran_4 = 0
+
 function log(msg) {
     console.log(msg)
 }
@@ -6,10 +11,10 @@ function randomFunction() {
     return Math.ceil(Math.random() * (0 - 6) + 6)
 }
 
-function switchImage() {
-    var image = $(".dice")
+function switchImage(position) {
+    var image = $(".dice" + position)
     count = 0
-    while (count < 10) {
+    while (count < 30) {
         image.animate({
             opacity: 0.5
         }, 15)
@@ -20,4 +25,49 @@ function switchImage() {
         })
         count++;
     }
+}
+
+function dices(position) {
+    if ($(".dice.hide" + position).length != 0) {
+        $(".dice.hide" + position).removeClass("hide")
+    }
+    switch (position) {
+        case ".um":
+            ran_1 = randomFunction()
+            log("1 " + ran_1)
+
+            $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_1 + '.png)')
+            break
+        case ".dois":
+            ran_2 = randomFunction()
+            log("2 " + ran_2)
+            $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_2 + '.png)')
+            break
+        case ".tres":
+            ran_3 = randomFunction()
+            log("3 " + ran_3)
+            $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_3 + '.png)')
+            break
+        case ".quatro":
+            ran_4 = randomFunction()
+            log("4 " + ran_4)
+            $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_4 + '.png)')
+            break
+        default:
+            console.error("erro")
+            break
+    }
+
+    switchImage(position)
+}
+
+function winner() {
+    ran_player = ran_1 + ran_2
+    ran_computer = ran_3 + ran_4
+    log(ran_1)
+    log(ran_2)
+    if (ran_player > ran_computer)
+        return "Jogador"
+    else
+        return "Computador"
 }
--- view/visual.php
+++ view/visual.php
@@ -1,18 +1,42 @@
-<div>
-    <button id="buttonClick" nome="buttonClick" class="buttonClick btn btn-primary">Rolar Dados</button>
-</div>
+<div class="container conteudo">
+    <div>
+        <button id="buttonClick" nome="buttonClick" class="buttonClick btn btn-primary">Rolar Dados</button>
+    </div>
+    <div class="row">
+        <div class="divDice col">
+            <label>Dado 1 Jogador</label>
+            <div class="dice um hide"></div>
+        </div>
+        <div class="divDice col">
+            <label>Dado 2 Jogador</label>
+            <div class="dice dois hide"></div>
+        </div>
+        <div class="divDice col">
+            <label>Dado 1 Computador</label>
+            <div class="dice tres hide"></div>
+        </div>
+        <div class="divDice col">
+            <label>Dado 2 Computador</label>
+            <div class="dice quatro hide"></div>
+        </div>
+    </div>
+    <div class="container containerWinner">
+        <label>O vencedor é</label>
 
-<div class="divDice">
-    <div class="dice hide"></div>
+        <div class="winner"></div>
+    </div>
 </div>
-
 <script>
     $(".buttonClick").click(()=>{
-        if($(".dice.hide").length != 0 ){
-            $(".dice.hide").removeClass("hide")
-        }
-        var ran = randomFunction()
-        switchImage()
-        $(".dice").css("background-image", 'url(./img/icons/dice_'+randomFunction()+'.png)')
+        dices(".um")
+
+        setTimeout(dices(".dois"), 10000)
+        
+        setTimeout(dices(".tres"), 10000)
+        
+        setTimeout(dices(".quatro"), 10000)
+
+        setInterval(10000)
+        $('.winner').text(winner())
     })
 </script>
