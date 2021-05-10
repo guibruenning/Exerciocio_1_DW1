08ce5ea21a4f8288ed8a093f15d1bc44af35925c
Bruenning <gui.h.bruenning@gmail.com>
2021-05-09 23:13:25 -0300

    ajsutado a animação dos dados, agora estão parando exatamente no valor que o jogador tirou

--- JS/functions.js
+++ JS/functions.js
@@ -13,6 +13,7 @@ function randomFunction() {
 
 function switchImage(position) {
     var image = $(".dice" + position)
+
     count = 0
     while (count < 30) {
         image.animate({
@@ -25,49 +26,62 @@ function switchImage(position) {
         })
         count++;
     }
+    return
+}
+
+function setRan(ran_1, ran_2, ran_3, ran_4) {
+    this.ran_1 = ran_1
+
+    this.ran_2 = ran_2
+
+    this.ran_3 = ran_3
+
+    this.ran_4 = ran_4
 }
 
 function dices(position) {
     if ($(".dice.hide" + position).length != 0) {
         $(".dice.hide" + position).removeClass("hide")
     }
-    switch (position) {
-        case ".um":
-            ran_1 = randomFunction()
-            log("1 " + ran_1)
-
-            $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_1 + '.png)')
-            break
-        case ".dois":
-            ran_2 = randomFunction()
-            log("2 " + ran_2)
-            $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_2 + '.png)')
-            break
-        case ".tres":
-            ran_3 = randomFunction()
-            log("3 " + ran_3)
-            $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_3 + '.png)')
-            break
-        case ".quatro":
-            ran_4 = randomFunction()
-            log("4 " + ran_4)
-            $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_4 + '.png)')
-            break
-        default:
-            console.error("erro")
-            break
-    }
-
     switchImage(position)
+
+    setTimeout(function() {
+
+        switch (position) {
+            case ".um":
+                log("1 " + ran_1)
+                $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_1 + '.png)')
+                break
+            case ".dois":
+                log("2 " + ran_2)
+                $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_2 + '.png)')
+                break
+            case ".tres":
+                log("3 " + ran_3)
+                $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_3 + '.png)')
+                break
+            case ".quatro":
+                log("4 " + ran_4)
+                $(".dice" + position).css("background-image", 'url(./img/icons/dice_' + ran_4 + '.png)')
+                break
+            default:
+                console.error("erro")
+                break
+        }
+    }, 1500)
+
+
 }
 
 function winner() {
     ran_player = ran_1 + ran_2
     ran_computer = ran_3 + ran_4
-    log(ran_1)
-    log(ran_2)
+    log(ran_computer)
+    log(ran_player)
     if (ran_player > ran_computer)
         return "Jogador"
-    else
+    if (ran_player < ran_computer)
         return "Computador"
+    else
+        return "Impate"
 }
--- view/visual.php
+++ view/visual.php
@@ -1,11 +1,12 @@
 <div class="container conteudo">
     <div>
-        <button id="buttonClick" nome="buttonClick" class="buttonClick btn btn-primary">Rolar Dados</button>
+        <button id="buttonClick" nome="buttonClick" class=" buttonClick btn btn-primary">Rolar Dados</button>
     </div>
     <div class="row">
         <div class="divDice col">
             <label>Dado 1 Jogador</label>
             <div class="dice um hide"></div>
+
         </div>
         <div class="divDice col">
             <label>Dado 2 Jogador</label>
@@ -21,22 +22,26 @@
         </div>
     </div>
     <div class="container containerWinner">
-        <label>O vencedor é</label>
+        <label>O vencedor é </label>
 
-        <div class="winner"></div>
+        <div class="winner fs-1 bg-primary bg-gradient"></div>
+
+        <label>com o Jogador tirando<b class="resultPlayer"></b> e o Computador tirando <b class="resultComputer"></b>
     </div>
 </div>
 <script>
+
     $(".buttonClick").click(()=>{
+        setRan(randomFunction(), randomFunction(), randomFunction(), randomFunction())
+
         dices(".um")
 
-        setTimeout(dices(".dois"), 10000)
+        setTimeout(dices, 2000, ".dois")
         
-        setTimeout(dices(".tres"), 10000)
+        setTimeout(dices, 4000, ".tres")
         
-        setTimeout(dices(".quatro"), 10000)
+        setTimeout(dices, 6000, ".quatro")
 
-        setInterval(10000)
-        $('.winner').text(winner())
+        setTimeout(function(){$('.winner').text(winner())},8000)
     })
 </script>
